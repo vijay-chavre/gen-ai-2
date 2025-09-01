70e43da8de7bc5eb489764e906be7b823b3782ff
Vijay Chavre <chavre.vijay@gmail.com>
2025-09-01 12:33:02 +0530

    Remove generated TypeScript declaration and JavaScript files from the dist directory

--- server/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Groq from "groq-sdk";
-export declare function main(): Promise<void>;
-export declare function getGroqChatCompletion(): Promise<Groq.Chat.Completions.ChatCompletion>;
-//# sourceMappingURL=index.d.ts.map
